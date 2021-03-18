dbc3e631816f2cf9e6b4894f52e2b0958c26a20a
pedrosc1967 <pedrosc1967@gmail.com>
2021-03-18 09:17:41 +0100

    v1.0.0  First version of the Diccionario de Panocho Fastfile corrected again Added correct Appfile

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,13 +10,14 @@ class Constants {
       'https://play.google.com/store/apps/dev?id=8134258655084518574';
   static const String URLOtherAppsIOS =
       'https://apps.apple.com/us/developer/fermina-soto-gonzlez/id1545741307';
-  static const String URLAplanetBitAndroid = 'https://aplanetbit.com';
-  static const String URLAplanetBitiOS = 'https://aplanetbit.com/?page_id=1359';
+  static const String URLAplanetBitAndroid =
+      'https://aplanetbit.com/?page_id=1362';
+  static const String URLAplanetBitiOS = 'https://aplanetbit.com/?page_id=1362';
   //static const String URLRateUsAndroid =
   //    'https://play.google.com/store/apps/details?id=com.aplanetbit.diccionario.panocho';
   //static const String URLRateUSIOS =
-  //    'https://apps.apple.com/us/app/dictionary-of-insurance/id1545741185?action=write-review';
+  //    'https://apps.apple.com/us/app/diccionario-panocho/id1545741185?action=write-review';
   static const String URLSentry =
       'https://243380c863dc4f0cbd6cc5fbbe6f22a7@o496488.ingest.sentry.io/5677937';
-  static const OneSignalAppID = 'b17d34fb-2e52-4e4c-994b-4897421a590d';
+  static const String OneSignalAppID = 'b17d34fb-2e52-4e4c-994b-4897421a590d';
 }
